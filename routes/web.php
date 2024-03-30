3d5eee92cf7ded72e1c992067bbe0f3dcb07c541
MuhamadAzmiNaziyulloh <azmi89017@gmail.com>
2024-03-30 23:00:01 +0700

    BelajarLaravel

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home', [PostController::class, 'home']);
-Route::get('/about', [PostController::class. 'about']);
+Route::get('/about', [PostController::class, 'about']);
 Route::get('/blog', [PostController::class, 'index'] );
 Route::get('posts/{slug}', [PostController::class, 'post']);
 Route::get('author/{slug}', [PostController::class, 'author']);
